16312a9e36351d486e4edd4242cd7c0516189f5c
Marty-jr-95 <martymuhangajr@outlook.com>
2021-07-17 14:15:31 +0200

    Update DocumentRow.js

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -6,7 +6,7 @@ const DocumentRow = ({id, fileName, date}) => {
 
     const router = useRouter();
     return (
-        <div onClick={() => router.push(`/doc/${id}`)} className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
+        <div onClick={() => router.push(`/doc/${id}`)} className="flex items-center p-4 mt-2 rounded-lg hover:bg-gray-100 text-gray-700 text-sm cursor-pointer">
             <Icon name="article" size="3xl" color="blue" />
             <p className="flex-grow pl-5 w-10 pr-10 truncate">{fileName}</p>
             <p className="pr-5 text-sm">{date?.toDate().toLocaleDateString()}</p>
