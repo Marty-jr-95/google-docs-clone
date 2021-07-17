80b4483e2585b18f3a9e0cf1da138f2a6b5c536b
Marty-jr-95 <martymuhangajr@outlook.com>
2021-07-17 14:19:24 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ export default function Home(props) {
 
      <section className="bg-white px-10 md:px-8">
        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
-          <div className="flex items-center justify-between pb-5">
+          <div className="flex items-center justify-between pb-5 border-b">
             <h2 className="font-medium flex-grow">My Documents</h2>
             <p className="mr-12">Date Created</p>
             <Icon name="folder" size="3xl" color="gray"/>
